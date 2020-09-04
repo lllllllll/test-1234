99acff4a824a19666826e2a6ef53d34c2f332536
YAKA <hnudka@gmail.com>
2020-09-04 14:40:56 +0700

    update structure

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { json } from 'body-parser';
 
+const port = process.env.PORT || 3000
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(
+    new ValidationPipe({ validationError: { target: false } }),
+  );
+  app.use(json({ limit: '50mb' }));
+  const options = new DocumentBuilder()
+    .setTitle('Never Sit Up')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('apis-doc', app, document);
+  await app.listen(port);
 }
 bootstrap();
